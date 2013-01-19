af8bdee52bbea37885f20b07396fb450d68a8ab2
freetrader <phil.balderson@mac.com>
2013-01-19 11:22:18 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkiApp::Application.config.secret_token = 'b52a4bf8b1f30ab4e6fd365c12f07924bcaa8298ca407667241625bcac644ced8f56846ad6ba1932230e0ccacce6b4c77c49cf605cbf0b166a6398323302f86a'
